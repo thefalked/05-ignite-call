2bc0ce088d9ce5e388e63f5989b9af3c4f78bb13
thefalked <giulianocrivelli@hotmail.com>
2023-01-08 22:57:07 -0400

    feat: adding scopes to google authentication and validating permissions

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -2,13 +2,38 @@ import { NextAuthOptions } from 'next-auth'
 import NextAuth from 'next-auth/next'
 import GoogleProvider from 'next-auth/providers/google'
 
+const GOOGLE_LINK = 'https://www.googleapis.com/auth'
+
+const googleScopeEmail = `${GOOGLE_LINK}/userinfo.email`
+const googleScopeProfile = `${GOOGLE_LINK}/userinfo.profile`
+const googleScopeCalendar = `${GOOGLE_LINK}/calendar`
+
+const googleScopes = [googleScopeEmail, googleScopeProfile, googleScopeCalendar]
+  .join(' ')
+  .trim()
+
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          scope: googleScopes,
+        },
+      },
     }),
   ],
+
+  callbacks: {
+    async signIn({ account }) {
+      if (!account?.scope?.includes(googleScopeCalendar)) {
+        return '/register/connect-calendar/?error=permissions'
+      }
+
+      return true
+    },
+  },
 }
 
 export default NextAuth(authOptions)
